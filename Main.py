1ff236e202794efe96d93acbd8ebafc4ada094e3
MaxvanderKroon <45490765+MaxvanderKroon@users.noreply.github.com>
2020-12-17 13:00:09 +0100

    imported MyRandomAgent as opponent to TTTSlayer800

--- Main.py
+++ Main.py
@@ -1,11 +1,10 @@
-from bke import start
+import random
+from bke import EvaluationAgent, start
 
 
-start()
-
-class TTTSlayer800 (EvaluatieAgent):
-    def evaluatie(self, bord, mijn_symbool, tegenstander_symbool):
-        return random.integer(1, 500)
+class MyRandomAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1, 500)
     
-TTT_Slayer_800 = TTTSlayer800()
-start(speler_o=TTT_Slayer_800)
+my_random_agent = MyRandomAgent()
+start(player_o=my_random_agent)
