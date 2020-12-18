fa1eeb11633af16cc8f2b8bf448255c85c12df12
MaxvanderKroon <45490765+MaxvanderKroon@users.noreply.github.com>
2020-12-18 10:50:38 +0100

    added TTTSlayer800 ai player

--- Main.py
+++ Main.py
@@ -1,10 +1,19 @@
 import random
-from bke import EvaluationAgent, start
+from bke import *
 
 
 class MyRandomAgent(EvaluationAgent):
     def evaluate(self, board, my_symbol, opponent_symbol):
         return random.randint(1, 500)
-    
+
+class TTTSlayer800(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        if can_win(board, opponent_symbol):
+            getal = getal - 1000
+        return getal
+            
+            
+ttt_slayer_800 = TTTSlayer800()
 my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent, player_x=my_random_agent)
+start(player_o=my_random_agent, player_x=ttt_slayer_800)
